d46e759cf5594ca191b54f83913a3abbe0d979c3
Mohamed <mohamedghaly14@gmail.com>
2020-07-18 03:13:42 +0200

    add drawer

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createTabNavigator } from 'react-navigation-tabs';
@@ -11,6 +13,19 @@ import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import CartScreen from '../screens/shop/CartScreen';
 
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+  },
+  headerTintColor: Platform.OS === 'android' ? '#fff' : Colors.primary,
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans',
+  },
+};
+
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
@@ -18,19 +33,47 @@ const ProductsNavigator = createStackNavigator(
     Cart: CartScreen,
   },
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-      },
-      headerTintColor: Platform.OS === 'android' ? '#fff' : Colors.primary,
-      headerTitleStyle: {
-        fontFamily: 'open-sans-bold',
-      },
-      headerBackTitleStyle: {
-        fontFamily: 'open-sans',
-      },
+    defaultNavigationOptions: defaultNavOptions,
+    navigationOptions: {
+      draweIcon: drawerConfig => (
+        <Ionicons
+          name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
     },
   }
 );
 
-export default createAppContainer(ProductsNavigator);
+const OrdersNavigator = createStackNavigator(
+  {
+    Orders: OrdersScreen,
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions,
+    navigationOptions: {
+      draweIcon: drawerConfig => (
+        <Ionicons
+          name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
+  }
+);
+
+const ShopNavigator = createDrawerNavigator(
+  {
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator,
+  },
+  {
+    contentOptions: {
+      activeTintColor: Colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(ShopNavigator);
--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Text, FlatList, Platform } from 'react-native';
+import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const OrdersScreen = () => {
-  return <View></View>;
+import HeaderButton from '../../components/UI/HeaderButton';
+
+const OrdersScreen = props => {
+  const orders = useSelector(state => state.orders.orders);
+  return (
+    <FlatList
+      data={orders}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => <Text>{item.totalAmount}</Text>}
+    />
+  );
+};
+
+OrdersScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'Your Orders',
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Menu'
+          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
