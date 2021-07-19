8614b0fb0b98d0e5c9fa24246d5d70a7ffd7f644
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-07-19 22:09:22 +0200

    upgrade sdk

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,104 +1,104 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const SIGNUP = 'SIGNUP';
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
-export const AUTHENTICATE = 'AUTHENTICATE';
+export const SIGNUP = "SIGNUP";
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+export const AUTHENTICATE = "AUTHENTICATE";
 
 export const authenticate = (userId, token) => {
-  return { type: AUTHENTICATE, userId: userId, token: token };
+	return { type: AUTHENTICATE, userId: userId, token: token };
 };
 
 export const signup = (email, password) => {
-  return async dispatch => {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD82C9qyC7K1C-_2OoDC9t0K2CwaX91R5o',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+	return async dispatch => {
+		const response = await fetch(
+			"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD82C9qyC7K1C-_2OoDC9t0K2CwaX91R5o",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: email,
+					password: password,
+					returnSecureToken: true,
+				}),
+			}
+		);
 
-    if (!response.ok) {
-      const errorResData = await response.json();
-      const errorId = errorResData.error.message;
-      let message;
-      if (errorId === 'EMAIL_EXISTS') {
-        message = 'The email address is already in use by another account.';
-      } else if (errorId === 'TOO_MANY_ATTEMPTS_TRY_LATER') {
-        message =
-          'We have blocked all requests from this device due to unusual activity. Try again later.';
-      }
-      throw new Error(message);
-    }
+		if (!response.ok) {
+			const errorResData = await response.json();
+			const errorId = errorResData.error.message;
+			let message;
+			if (errorId === "EMAIL_EXISTS") {
+				message = "The email address is already in use by another account.";
+			} else if (errorId === "TOO_MANY_ATTEMPTS_TRY_LATER") {
+				message =
+					"We have blocked all requests from this device due to unusual activity. Try again later.";
+			}
+			throw new Error(message);
+		}
 
-    const resData = await response.json();
+		const resData = await response.json();
 
-    dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
-    const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn) * 1000
-    );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
-  };
+		dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+		const expirationDate = new Date(
+			new Date().getTime() + parseInt(resData.expiresIn) * 1000
+		);
+		saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+	};
 };
 
 export const login = (email, password) => {
-  return async dispatch => {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD82C9qyC7K1C-_2OoDC9t0K2CwaX91R5o',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+	return async dispatch => {
+		const response = await fetch(
+			"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD82C9qyC7K1C-_2OoDC9t0K2CwaX91R5o",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: email,
+					password: password,
+					returnSecureToken: true,
+				}),
+			}
+		);
 
-    if (!response.ok) {
-      const errorResData = await response.json();
-      const errorId = errorResData.error.message;
-      let message;
-      if (errorId === 'EMAIL_NOT_FOUND') {
-        message = 'This email could not be found! Maybe Sign Up';
-      } else if (errorId === 'INVALID_PASSWORD') {
-        message = 'This password is not valid!';
-      }
-      throw new Error(message);
-    }
+		if (!response.ok) {
+			const errorResData = await response.json();
+			const errorId = errorResData.error.message;
+			let message;
+			if (errorId === "EMAIL_NOT_FOUND") {
+				message = "This email could not be found! Maybe Sign Up";
+			} else if (errorId === "INVALID_PASSWORD") {
+				message = "This password is not valid!";
+			}
+			throw new Error(message);
+		}
 
-    const resData = await response.json();
+		const resData = await response.json();
 
-    dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
-    const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn) * 1000
-    );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
-  };
+		dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+		const expirationDate = new Date(
+			new Date().getTime() + parseInt(resData.expiresIn) * 1000
+		);
+		saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+	};
 };
 
 export const logout = () => {
-  return { type: LOGOUT };
+	return { type: LOGOUT };
 };
 
 export const saveDataToStorage = (token, userId, expirationDate) => {
-  AsyncStorage.setItem(
-    'userData',
-    JSON.stringify({
-      token: token,
-      userId: userId,
-      expiryDate: expirationDate.toISOString(),
-    })
-  );
+	AsyncStorage.setItem(
+		"userData",
+		JSON.stringify({
+			token: token,
+			userId: userId,
+			expiryDate: expirationDate.toISOString(),
+		})
+	);
 };
